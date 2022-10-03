196bd406f3abe8bcae81408c7f8111192d734932
MohanReddy000 <mr881789@gmail.com>
2022-10-03 12:42:22 +0550

    feat: Initialize App

---
+++ erpnext_mock_project/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "erpnext_mock_project",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("erpnext_mock_project")
+		}
+	]
