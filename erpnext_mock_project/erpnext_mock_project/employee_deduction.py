365ce58c13abb9af0e52d9a377c9f6a314c78d9c
MohanReddy000 <114720035+MohanReddy000@users.noreply.github.com>
2022-10-07 20:53:25 +0530

    Add files via upload

---
+++ erpnext_mock_project/erpnext_mock_project/employee_deduction.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2022, erpnext_mock_project and contributors
+# For license information, please see license.txt
+import frappe
+#from frappe.model.document import Document
+import datetime
+
+@frappe.whitelist()
+def deduction(name):
+    data = frappe.db.sql(""" select DISTINCT employee_name from `tabEmployee` where name=%s """,(name),as_dict=1)
+    return data
+@frappe.whitelist()
+def convertDateFormat(start_date):
+    start_date=str(start_date)
+    date=datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    return date.strftime('%b-%y')
